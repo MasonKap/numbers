c35ae6f4c936c7174297990b885125c4f2e966a2
MasonKap <makaplan@chapman.edu>
2020-12-07 15:46:27 -0800

    Updated numbers.sh

--- numbers.sh
+++ numbers.sh
@@ -1,16 +1,17 @@
 #! /bin/bash
-#this is a script called numbers.sh
-#By Mason Kaplan
-read -p 'Enter a positive number: ' number
+#this is a script called numbers.sh woooooo
+#by mason kaplan
+echo 'Enter a positive number: ' 
+read -r number
 x=1
-while [ $number -ge $x ]
-  do
-  out=$(( $number % 2 ))
-  if [ $out -eq 0 ]
-  then
-    echo "$number Even"
-  else
-    echo "$number Odd"
-  fi
-number=$(($number-1))
+while [ "$number" -ge "$x" ]
+	do
+	out=$((number%2))
+	if [ $out -eq 0 ]
+	then
+		echo "$number Even"
+	else
+		echo "$number Odd"
+	fi
+number=$((number-1))
 done
